c22696c3917f86d986935359a9a945af4886f93a
BrightLkms672 <nouvelle672@gmail.com>
2015-08-16 17:08:00 -0700

    Encrypt2.0 GENERATOR

--- script/app.js
+++ script/app.js
@@ -1,46 +1,64 @@
-$('#encryptButn').click(arrangeString)
-$('#dencryptButn').click(reverseString)
-function arrangeString(){
+$('.encryptButn').click({param1: "enc"}, arrangeString);
+$('.dencryptButn').click({param2: "dec"}, arrangeString);
+
+function arrangeString(funct){
 	var pass = document.getElementById('passInput').value
-	var passnumber = 0
+	var passlist = [] 
 	for (var i=0;i<pass.length;i++){
-		passnumber += pass.charCodeAt(i)
+		if (typeof pass[i] == "string"){
+			passlist.push(pass.charCodeAt(i))
+		}
+		else{
+			passlist.push(pass[i])
+		}
 	}
-	var newString = document.getElementById('inputText').value
-	document.getElementById('inputText').value = fetchAscii(newString, passnumber)
-	$('#passInput').value = ""
+	var text = document.getElementById('inputText').value
+	if (funct.data.param1 == "enc"){
+		document.getElementById('inputText').value = fetchAscii(text, passlist)
+		document.getElementById('passInput').value = "" 
+	}
+	else if(funct.data.param2 == "dec"){
+		var newText = text.split("&")	
+		document.getElementById('inputText').value = fetchAscii2(newText, passlist)
+		document.getElementById('passInput').value =""
+	}
 }
 
-function reverseString(){
-	var pass = document.getElementById('passInput').value
-	var passnumber = 0
-	for (var i=0;i<pass.length;i++){
-		passnumber += pass.charCodeAt(i)
+function* infiniteGen(listt){
+	var i = 0
+	length = listt.length
+	while (listt.length > 0){
+		if (i == (length-1)){
+			yield (listt[i])
+			i = 0
+		}
+		else{
+			yield (listt[i])
+			i += 1
+		}
 	}
-	var oldString = document.getElementById('inputText').value
-	var newString = oldString.split("&")
-	document.getElementById('inputText').value = fetchAscii2(newString, passnumber)
-	$('#passInput').value = ""
 }
-
 function fetchAscii(obj, pass)
 {
       var convertedObj = '';
+      b = infiniteGen(pass)
       for(i = 0; i < obj.length; i++)
       { 
-            var asciiChar = obj.charCodeAt(i) +35+ pass;
+            var asciiChar = obj.charCodeAt(i) + 10 + b.next().value;
             convertedObj += '&' + asciiChar;
       } 
       return convertedObj;
 }
-
 function fetchAscii2(obj, pass)
 {
       var convertedObj = '';
+      c = infiniteGen(pass)
+      console.log(typeof obj)
+      obj.shift()
       for(i = 0; i < obj.length; i++)
       { 
-            var Char = String.fromCharCode(obj[i]-35-pass);
-            convertedObj += Char;
+            var letter = String.fromCharCode((obj[i]) - 10 - c.next().value);    
+            convertedObj += letter;
       } 
       return convertedObj;
 }
